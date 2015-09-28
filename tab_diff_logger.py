90890e377d4417c00e1a09e1c459450269d6bc79
Phil Bozak <bozak@amazon.com>
2015-09-28 07:51:07 -0700

    Only write n_tabs.json every minute

--- tab_diff_logger.py
+++ tab_diff_logger.py
@@ -12,7 +12,7 @@ OUTPUT_PATH = "tab_history.log"
 class TabSet:
     def __init__(self, init_set=set()):
         self.items = init_set
-        
+
     def add_diff(self, differences):
         """ Should be given an iterable of strings where the string is expected to start with '-' or '+' """
         for d in differences:
@@ -24,7 +24,7 @@ class TabSet:
                 self.items.add(d[1:])
             else:
                 raise RuntimeError("Diff string didn't start with '+' or '-'")
-    
+
     def diff_with(self, other_set):
         plus_set = other_set - self.items
         minus_set = self.items - other_set
@@ -34,11 +34,11 @@ class TabSet:
         for removeme in minus_set:
             output.append("-"+removeme)
         return output
-        
+
 class TabDiffLoader:
     def __init__(self, filename):
         self.filename = filename
-    
+
     def load(self):
         """ Returns the current TabSet """
         touch = open(self.filename, 'a')
@@ -54,11 +54,11 @@ class TabDiffLoader:
             ts.add_diff(l[:-1] for l in lines[start:stop])  # remove trailing newline
             start = stop + 1
         return ts
-        
+
 class TabDiffWriter:
     def __init__(self, filename):
         self.filename = filename
-    
+
     def write(self, tab_set, other_set):
         """ Writes the differences between the two tab sets to the file """
         lines_to_write = [diff_line+"\n" for diff_line in tab_set.diff_with(other_set)]
@@ -67,11 +67,11 @@ class TabDiffWriter:
         with open(self.filename, 'a') as f:
             f.write("=%d\n" % time.time())
             f.writelines(lines_to_write)
-            
+
 class SessionStoreLoader:
     def __init__(self, path_to_session_store):
         self.path = path_to_session_store
-        
+
     def get_open_tab_ids(self):
         jObj = json.load(open(self.path, 'r'))
         for window in jObj["windows"]:
@@ -85,26 +85,31 @@ class ConfigLoader:
 
     def get_value(self, key):
         return self.config[key]
-        
+
 def update_with_current_tabs():
     ff_directory = ConfigLoader().get_value("firefox_directory")
-    session_store_path = os.path.join(ff_directory, "sessionstore.js")
+    session_store_path = os.path.join(ff_directory, "sessionstore-backups", "recovery.js")
     ssl = SessionStoreLoader(session_store_path)
     curr_tabs = set(ssl.get_open_tab_ids())
-    
+
     loader = TabDiffLoader(OUTPUT_PATH)
     ts = loader.load()
-    
+
     writer = TabDiffWriter(OUTPUT_PATH)
     writer.write(ts, curr_tabs)
 
 def main():
     print("Running tabdiff...")
+    count_tabs_counter = 0
     while True:
         update_with_current_tabs()
-        count_open_tabs.run(OUTPUT_PATH, "n_tabs.json")
+        if count_tabs_counter == 0:
+            count_open_tabs.run(OUTPUT_PATH, "n_tabs.json")
+            count_tabs_counter += 1
+        if count_tabs_counter >= 4:
+            count_tabs_counter = 0
         print("[%s] Finished diffing and converting to JSON" % time.strftime("%H:%M:%S"))
         time.sleep(15)  # the session store is written to every 15 seconds
-        
+
 if __name__ == "__main__":
     main()
